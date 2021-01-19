f177865af625b4d0305a6d69106ee616f41f0f0b
cohyou <cohyou@aurola.net>
2021-01-19 17:47:58 +0000

    update for today

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,8 @@ const IndexPage = () => (
     </div>
     <Link to="/page-2/">2021-01-16 まず、最初のごあいさつ</Link> <br />
     <Link to="/20210117/">2021-01-17 音楽環境を整える</Link> <br />
+    <Link to="/20210118/">2021-01-18 スタジオ、GEN 20:00~23:00</Link> <br />
+    <Link to="/20210119/">2021-01-19 通常更新、初日</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
