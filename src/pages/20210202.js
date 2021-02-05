063e2bd23c1c02e64f7aab31f878bdf4aee3cb1b
cohyou <cohyou@aurola.net>
2021-02-05 14:16:34 +0000

    update

---
+++ src/pages/20210202.js
@@ -0,0 +1,30 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const SecondPage = () => (
+  <Layout>
+    <SEO title="もはや覚えていない" />
+    <h1>2021-02-02</h1>
+    <h2>もはや覚えていない</h2>
+    <p>
+      おととい（つまりこれは2/4に書いているわけです、すいません）
+      仕事は、火曜なのでABテスト1.5回リリース、でも前日が関係ないやつのまーじミスに全体が巻き込まれて潰れたので、
+      その続きを急いでマージして動作確認してリリース準備してました。その途中でさらに六本木さんの修正とコンフリクトが発覚して、
+      さらに修正をすることに。
+      
+      でも結構遅くまでいたので、カレーしか食えなかった。仕事しながら食った
+
+      正直あまり覚えていないがあっというまに夜遅くなっていた記憶もあるしゲームせず寝てた記憶もある。だから水曜日は元気でしたけど
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <Link to="/">目次に戻る</Link>
+  </Layout>
+)
+
+export default SecondPage
---
+++ src/pages/20210203.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const SecondPage = () => (
+  <Layout>
+    <SEO title="久々の低血糖" />
+    <h1>2021-02-03</h1>
+    <h2>久々の低血糖</h2>
+    <p>
+      仕事が、午前中にミスをしていたことが発覚した（vパラメータを更新忘れていた
+      昨日は本当に疲れ切って、外に買い物にもいけず、ちょっとぐったりして低血糖でカレーをなんとか食った。
+      でも夜中は初音ミクやってた。昨日はさすがにしょうがない感じある。
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <Link to="/">目次に戻る</Link>
+  </Layout>
+)
+
+export default SecondPage
